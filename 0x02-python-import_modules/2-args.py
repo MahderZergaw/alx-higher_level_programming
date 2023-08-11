cbfb887d03d955577c85011d4ace806f535c360e
magizergi <magizergi19@gmail.com>
2023-08-11 14:20:04 +0300

    python high

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+
 import sys
 
 if __name__ == '__main__':
-    arguments = sys.argv[1:]  # Exclude the script name
+    num_arguments = len(sys.argv) - 1
 
-    num_arguments = len(arguments)
     if num_arguments == 0:
         print("Number of argument(s): .")
     elif num_arguments == 1:
-        print("Number of argument(s): 1: {0}".format(arguments[0]))
+        print("Number of argument(s): 1: {0}".format(sys.argv[1]))
     else:
-        print("Number of argument(s): {0}: {1}".format(num_arguments, ", ".join(arguments)))
-
-    if num_arguments > 0:
-        print()
-        for idx, arg in enumerate(arguments, start=1):
-            print("{0}: {1}".format(idx, arg))
+        print("Number of argument(s): {0}:".format(num_arguments))
+        for i in range(1, num_arguments + 1):
+            print("{0}: {1}".format(i, sys.argv[i]))
