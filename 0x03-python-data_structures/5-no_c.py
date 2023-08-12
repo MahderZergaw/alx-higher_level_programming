002c5819194c22f9f57710dd2e89bc6cfdaf96ef
magizergi <magizergi19@gmail.com>
2023-08-12 10:26:19 +0300

    New Commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = "".join([char for char in my_string if char.upper() != "C"])
-    return (new_string)
-print(no_c("No more C. Come and Code along with me"))
+    new = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            new += my_string[i]
+    return new
