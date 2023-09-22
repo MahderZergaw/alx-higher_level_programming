6b454e6b26b2b193c93a29aa71c19f1fb683ef14
magizergi <magizergi19@gmail.com>
2023-09-22 09:11:00 +0300

    new update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,83 +1,48 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+"""Square class based on Rectangle"""
+
+import json
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """Defining Square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
+        """Instanitating an object
+        Attrs:
+            size (int) -> size of the square
+            x (int)    -> x-axis offset
+            y (int)    -> y-axis offset
+            id(int)    -> Unique identifier
         """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """str() representation of an object"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id,
+                self.x, self.y, self.width))
+
     @property
     def size(self):
-        """Get/set the size of the Square."""
-        return self.width
+        """Getter for size"""
+        return self.size
 
     @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
+    def size(self, val):
+        """Setter for size"""
+        self.width = val
+        self.height = val
 
     def update(self, *args, **kwargs):
-        """Update the Square.
-
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        """Update an instance's attributes"""
+        if args is not None and len(args) is not 0:
+            attributes = ['__id', '__width', '__height', '__x', '__y']
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
+        """Serialize an object of Square class"""
+        return json.dumps(self)
