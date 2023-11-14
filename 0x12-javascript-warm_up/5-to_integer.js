f4e10782bd834da491fe5b9eaa915833545be1ef
MahderZergaw <mahderzergaw640@gmail.com>
2023-11-14 10:16:34 -0800

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-const argument = process.argv.slice(2);
-const number = parseInt(argument[0]);
-if (Number.isInteger(number)) { console.log('My number: ', number); } else { console.log('Not a number'); }
+
+const arg = process.argv[2];
+const number = parseInt(arg, 10);
+
+if (Number.isInteger(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
